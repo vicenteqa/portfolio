ac2f184c36181fe3dc7eb4d4eea4c2bd1e2de4ff
Vicente <virucu@gmail.com>
2024-10-02 12:54:19 +0200

    add visual test

--- tests/home-page.spec.ts
+++ tests/home-page.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('Portfolio has the expected title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/');
+});
+test('Portfolio has the expected title', async ({ page }) => {
   await expect(page).toHaveTitle(/Vicente Ruiz - Portfolio/);
+});
+
+test('Home has the expected appearance', async ({ page }) => {
+  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.05 });
 });
